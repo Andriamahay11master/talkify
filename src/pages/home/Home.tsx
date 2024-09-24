3b94c83b1c114c9c0fba80feabb95b29cf1acbc4
Andriamahay11master <hirimanana@yahoo.fr>
2024-09-24 16:17:22 +0300

    add page home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,18 @@
+import { Link } from 'react-router-dom'
 import './home.scss'
 export default function Home() {
-    return <div>Home</div>
+    return (
+        <div className='main-page page-home'>
+            <div className="main-logo">
+                <img src="/logo-talkify.png" alt="Logo Talkify" className="logo" />
+            </div>
+            <div className="main-welcome">
+                <h1 className="title-h1">Welcome to Talkify</h1>
+                <p>
+                    Read our <Link className="btn btn-link" to="/privacy-policy">Privacy Policy</Link>. Tap “Agree and Continue” to accept.
+                </p>
+                <Link className="btn btn-secondary" to="/signIn">Agree and Continue</Link>
+            </div>
+        </div>
+    )
 }
