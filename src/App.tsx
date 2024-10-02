793ef587f896e965f102e195bfc70571aea5b6a5
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-02 16:01:12 +0300

    add console log

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ function App() {
     };
     fetchData();
       const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+        console.log("Event", event)
         if (session) {
             setUser(session.user); // L'utilisateur est connecté
         } else {
