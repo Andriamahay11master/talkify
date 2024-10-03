d7c522855de08312ba6df464f5644f401b236075
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-03 09:44:20 +0300

    add samples images users

--- src/data/chat.ts
+++ src/data/chat.ts
@@ -1,18 +1,18 @@
 export const listChat = [
     {
-        imgSrc: '/user.png',
+        imgSrc: '/users-1.jpg',
         name: 'Ali Tahir',
         date: '10:43 AM',
         text: 'Hello, how are you ?'
     },
     {    
-        imgSrc: '/user.png',
+        imgSrc: '/users-2.jpg',
         name: 'Crush',
         date: '9:43 AM',
         text: 'Will do, super, thank you ?'
     },
     {    
-        imgSrc: '/user.png',
+        imgSrc: '/users-3.jpg',
         name: 'Bhai',
         date: '7:30 AM',
         text: 'File Uploaded'
